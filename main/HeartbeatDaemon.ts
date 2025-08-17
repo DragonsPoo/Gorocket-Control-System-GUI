11fb4628f1722d2348a9a41f701a7f7b9b3a049f
JunghoShin <jungho1902@korea.ac.kr>
2025-08-17 12:51:09 +0900

    Create HeartbeatDaemon.ts

---
+++ main/HeartbeatDaemon.ts
@@ -0,0 +1,26 @@
+import type { SerialManager } from './SerialManager';
+
+export class HeartbeatDaemon {
+  private timer: NodeJS.Timeout | null = null;
+  private intervalMs: number;
+
+  constructor(private serial: SerialManager, intervalMs = 200) {
+    this.intervalMs = intervalMs;
+  }
+
+  start(intervalMs?: number) {
+    if (typeof intervalMs === 'number' && intervalMs > 0) {
+      this.intervalMs = intervalMs;
+    }
+    this.stop();
+    this.timer = setInterval(() => {
+      // HB는 실패해도 조용히 무시. 연결 안 되어 있으면 send가 reject됨.
+      this.serial.send({ raw: 'HB' }).catch(() => {});
+    }, this.intervalMs);
+  }
+
+  stop() {
+    if (this.timer) clearInterval(this.timer);
+    this.timer = null;
+  }
+}
